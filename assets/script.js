d649e38dbd126808bf2472046b75ef8289122a56
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-10-01 16:52:10 -0500

    added function for colors to change throughout the day

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,28 @@
+$(document).ready(function(){
+    
+    var todayDate = moment().format('dddd, MMM Do YYYY');
+    $("#currentDay").html(todayDate);
+    
+    function timeBlockUpdater() {
+        var currentHour = moment().hours()
 
-let today = moment();
-$("#1a").text(today.format("MMM Do, YYYY"));
+        $('.time-block').each(function(){
+            var timeBlockHour = parseInt($(this).attr('id'))
+            console.log(timeBlockHour)
 
+            if(timeBlockHour<currentHour){
+                $(this).addClass('past')
+            } else if (timeBlockHour === currentHour) {
+                $(this).removeClass('past')
+                $(this).addClass('present')
+            } else {
+                $(this).removeClass('past')
+                $(this).removeClass('present')
+                $(this).addClass('future')
+            }
+        })
+    }
+    
+    timeBlockUpdater()
+
+})
