bb10fdf66a5aaea303660df0f5537d3d10982773
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-10-02 12:42:42 -0500

    condensed local storage into a for loop

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
+
+
 $(document).ready(function () {
+  // moment JS used to update the date everyday to current date
   let todayDate = moment().format("dddd, MMM Do YYYY");
   $("#currentDay").html(todayDate);
 
@@ -7,6 +10,9 @@ $(document).ready(function () {
     $(".time-block").each(function () {
       let timeBlockHour = parseInt($(this).attr("id"));
       console.log(timeBlockHour);
+
+
+      // if statment used for color change throughout the day 
 
       if (timeBlockHour < currentHour) {
         $(this).addClass("past");
@@ -19,6 +25,8 @@ $(document).ready(function () {
         $(this).addClass("future");
       }
 
+      // saves local storage 
+
       $(".saveBtn").on("click", function () {
         console.log("saveBtn");
         let time = $(this).parent().attr("id");
@@ -30,14 +38,10 @@ $(document).ready(function () {
   }
   timeBlockUpdater();
 
-  $("#8 .description").val(localStorage.getItem("8"));
-  $("#9 .description").val(localStorage.getItem("9"));
-  $("#10 .description").val(localStorage.getItem("10"));
-  $("#11 .description").val(localStorage.getItem("11"));
-  $("#12 .description").val(localStorage.getItem("12"));
-  $("#13 .description").val(localStorage.getItem("13"));
-  $("#14 .description").val(localStorage.getItem("14"));
-  $("#15 .description").val(localStorage.getItem("15"));
-  $("#16 .description").val(localStorage.getItem("16"));
-  $("#17 .description").val(localStorage.getItem("17"));
+// for loop for local storage 
+
+for (let i = 8; i<=17; i++){
+  $(`#${i} .description`).val(localStorage.getItem(`${i}`));
+}
+
 });
