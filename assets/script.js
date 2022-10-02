c76878d482a48dcb65ce3faa66e09f7a75826ee1
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-10-01 19:18:16 -0500

    formatted HTML_CSS_JAVASCRIPT Files

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  let todayDate = moment().format("dddd, MMM Do YYYY");
+  $("#currentDay").html(todayDate);
 
-    // Displays today's day and date
-    var todayDate = moment().format('dddd, MMM Do YYYY');
-    $("#currentDay").html(todayDate);
-    
+  function timeBlockUpdater() {
+    let currentHour = moment().hours();
+    $(".time-block").each(function () {
+      let timeBlockHour = parseInt($(this).attr("id"));
+      console.log(timeBlockHour);
 
-    // Functions to save tasks by clicking on the save button.
-    $(".saveBtn").on("click", function() {
-        console.log("saveBtn")
-        var time = $(this).parent().attr('id')
-        var text = $(this).siblings(".description").val()
-        console.log(time, text)
-        localStorage.setItem(time,text)
-    })
-    // Function to check the time and change the color
-    function timeBlockUpdater() {
-        var currentHour = moment().hours()
+      if (timeBlockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
 
-        $('.time-block').each(function(){
-            var timeBlockHour = parseInt($(this).attr('id'))
-            console.log(timeBlockHour)
+      $(".saveBtn").on("click", function () {
+        console.log("saveBtn");
+        let time = $(this).parent().attr("id");
+        let text = $(this).siblings(".description").val();
+        console.log(time, text);
+        localStorage.setItem(time, text);
+      });
+    });
+  }
+  timeBlockUpdater();
 
-            if(timeBlockHour<currentHour){
-                $(this).addClass('past')
-            } else if (timeBlockHour === currentHour) {
-                $(this).removeClass('past')
-                $(this).addClass('present')
-            } else {
-                $(this).removeClass('past')
-                $(this).removeClass('present')
-                $(this).addClass('future')
-            }
-            $(".saveBtn").on("click", function() {
-                console.log("saveBtn")
-                var time = $(this).parent().attr('id')
-                var text = $(this).siblings(".description").val()
-                console.log(time, text)
-                localStorage.setItem(time,text)
-            })
-        })
-    }
-    
-    timeBlockUpdater()
-
-    $('#8 .description').val(localStorage.getItem('8'))
-    $('#9 .description').val(localStorage.getItem('9'))
-    $('#10 .description').val(localStorage.getItem('10'))
-    $('#11 .description').val(localStorage.getItem('11'))
-    $('#12 .description').val(localStorage.getItem('12'))
-    $('#13 .description').val(localStorage.getItem('13'))
-    $('#14 .description').val(localStorage.getItem('14'))
-    $('#15 .description').val(localStorage.getItem('15'))
-    $('#16 .description').val(localStorage.getItem('16'))
-    $('#17 .description').val(localStorage.getItem('17'))
-
-})
+  $("#8 .description").val(localStorage.getItem("8"));
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+});
