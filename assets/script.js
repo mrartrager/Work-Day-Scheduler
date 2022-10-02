621193f389d82e8b0e1532a44ba240b0b0979a4a
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-10-01 19:14:32 -0500

    finally got data to store after refresh and added save data in click function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-    
+
+    // Displays today's day and date
     var todayDate = moment().format('dddd, MMM Do YYYY');
     $("#currentDay").html(todayDate);
     
+
+    // Functions to save tasks by clicking on the save button.
+    $(".saveBtn").on("click", function() {
+        console.log("saveBtn")
+        var time = $(this).parent().attr('id')
+        var text = $(this).siblings(".description").val()
+        console.log(time, text)
+        localStorage.setItem(time,text)
+    })
+    // Function to check the time and change the color
     function timeBlockUpdater() {
         var currentHour = moment().hours()
 
@@ -20,9 +31,27 @@ $(document).ready(function(){
                 $(this).removeClass('present')
                 $(this).addClass('future')
             }
+            $(".saveBtn").on("click", function() {
+                console.log("saveBtn")
+                var time = $(this).parent().attr('id')
+                var text = $(this).siblings(".description").val()
+                console.log(time, text)
+                localStorage.setItem(time,text)
+            })
         })
     }
     
     timeBlockUpdater()
+
+    $('#8 .description').val(localStorage.getItem('8'))
+    $('#9 .description').val(localStorage.getItem('9'))
+    $('#10 .description').val(localStorage.getItem('10'))
+    $('#11 .description').val(localStorage.getItem('11'))
+    $('#12 .description').val(localStorage.getItem('12'))
+    $('#13 .description').val(localStorage.getItem('13'))
+    $('#14 .description').val(localStorage.getItem('14'))
+    $('#15 .description').val(localStorage.getItem('15'))
+    $('#16 .description').val(localStorage.getItem('16'))
+    $('#17 .description').val(localStorage.getItem('17'))
 
 })
